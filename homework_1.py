08e60d84bb5fccf862792d9f5183f751b130a6d0
Chyntemir <iskitovakenje@gmail.com>
2025-08-01 21:50:07 +0600

    homework_1

--- homework_1.py
+++ homework_1.py
@@ -5,9 +5,9 @@ class Person:
         self.occupation = occupation
         self.higher_education = higher_education
 
-person_1 = Person("Daniel", 2000,  "writer",  True)
-person_2 = Person("Islam",  1998, "coder",  True)
-person_3 = Person("Bael",  2007,  "student",  False)
-print(f'name: {person_1.name}, birth date: {person_1.birth_date}, occupation: {person_1.occupation}, availability of higher education: {person_1.higher_education}')
-print(f'name: {person_2.name}, birth date: {person_2.birth_date}, occupation: {person_2.occupation}, availability of higher education: {person_2.higher_education}')
-print(f'name: {person_3.name}, birth date: {person_3.birth_date}, occupation: {person_3.occupation}, availability of higher education: {person_3.higher_education}')
+person_daniel = Person("Daniel", 2000,  "writer",  True)
+person_islam = Person("Islam",  1998, "coder",  True)
+person_bael = Person("Bael",  2007,  "student",  False)
+print(f'name: {person_daniel.name}, birth date: {person_daniel.birth_date}, occupation: {person_daniel.occupation}, availability of higher education: {person_daniel.higher_education}')
+print(f'name: {person_islam.name}, birth date: {person_islam.birth_date}, occupation: {person_islam.occupation}, availability of higher education: {person_islam.higher_education}')
+print(f'name: {person_bael.name}, birth date: {person_bael.birth_date}, occupation: {person_bael.occupation}, availability of higher education: {person_bael.higher_education}')
