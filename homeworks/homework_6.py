76cdcc4e084a7c2571f00647b90fd15e524fb6ef
Chyntemir <iskitovakenje@gmail.com>
2025-08-19 21:54:34 +0600

    homework_6

---
+++ homeworks/homework_6.py
@@ -0,0 +1,11 @@
+import blessed
+
+term = blessed.Terminal()
+
+print(term.red3 + 'Apple 🍎' + term.normal)
+print(term.olivedrab3 + 'Banana 🍌' + term.normal)
+print(term.darkgoldenrod1 + 'Cherry 🍒' + term.normal)
+print(term.turquoise3 + 'Grape 🍇' + term.normal)
+print(term.violet + 'Mango 🥭' + term.normal)
+print(term.skyblue + 'Orange 🍊' + term.normal)
+print(term.snow + 'Peach 🍑' + term.normal)
